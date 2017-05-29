18f7270ac83a87834c4a5e0aecca4b7decce5e5d
CostinaB <costina.baticaa@gmail.com>
2017-05-29 12:40:52 +0300

    Add files via upload

--- SpecLetter.js
+++ SpecLetter.js
@@ -10,20 +10,37 @@ it("should be able to create a deck", function() {
 	expect(pachet).toEqual(deck);
 });
 it("should be able to return second player's card", function() {
-	var card = Script.strategieCalculator(2, 5);
-	expect(card).toEqual(2);
+	var card = Script.strategieCalculator(3, 4);
+	expect(card).toEqual(4);
 });
+beforeEach(function() {
+    PrintesaJ = jasmine.createSpy("PrintesaJ");    
+    PrintesaJ("called as usual");
+});
+
 it("should be able to return printesa img", function() {
 	var printesa = Script.printesa_8();
-	expect(printesa.isPresent()).toBe(true);
+var x = document.createElement("img");
+	x.setAttribute("src", "Poze/printesa.JPG");
+	x.setAttribute("alt", "Printesa");
+	x.setAttribute("id", "printesa");
+expect(x).toEqual(printesa);
+
 });
-it("should be able to return printesa img", function() {
+
+it("should be able to return rege img", function() {
 	var rege = Script.rege_6();
-	expect(rege.isPresent()).toBe(true);
+var x = document.createElement("img");
+	x.setAttribute("src", "Poze/rege.JPG");
+	x.setAttribute("alt", "Rege");
+	expect(x).toEqual(rege);
 });
-it("should be able to return printesa img", function() {
+it("should be able to return print img", function() {
 	var print = Script.print_5();
-	expect(print.isPresent()).toBe(true);
+	var x = document.createElement("img");
+	x.setAttribute("src", "Poze/print.JPG");
+	x.setAttribute("alt", "Print");
+expect(x).toEqual(print);
 });
 
 });
